526d25b708f9ffe6dce061699ce67ec8fddda236
INnoVatioN-97 <koyoungil97@gmail.com>
2023-04-02 18:55:50 +0900

    env 파일 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+import { ConfigModule } from '@nestjs/config';
+import { User } from './user/user.entity';
+import { UserModule } from './user/user.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      // host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}

6e5c7839967e13550ee17212ed3bf8cf568f617a
INnoVatioN-97 <koyoungil97@gmail.com>
2023-04-08 17:40:16 +0900

    생기초적인 GET, PORT 요청, RequestBody

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { v1 as uuid } from 'uuid';
+import { Board, BoardStatus } from './boards.model';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boardList: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boardList;
+  }
+
+  createBoard(title: string, description: string) {
+    const board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boardList.push(board);
+  }
+}
